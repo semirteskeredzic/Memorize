7dbddb88c477522345f03465d58a863f4eec8458
semirteskeredzic <semir.teskeredzic@gmail.com>
2024-12-16 21:53:26 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Semir Teskeredzic on 16. 12. 24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
